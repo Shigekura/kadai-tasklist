ab3089cbfe62fc51bec758772f49b93288a43664
shigekura2021 <shigekura2021@gmail.com>
2021-05-21 07:41:08 +0000

    correct private method location

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,10 +43,11 @@ class TasksController < ApplicationController
         flash[:success] = 'このお仕事はお仕事リストから正常に削除されました。お疲れさまでした。'
         redirect_to tasks_url
   end
-end
 
 private
 # Strong Parameter
   def task_params
         params.require(:task).permit(:content)
   end
+  
+end
