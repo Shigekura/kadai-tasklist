799bd3ec809e43b661c7c5b666205482dbcdba01
shigekura2021 <shigekura2021@gmail.com>
2021-05-22 22:20:35 +0000

    add column&validation, ruby version

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -23,11 +24,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'お仕事の情報が正常に修正されました'
       redirect_to @task
@@ -38,16 +37,21 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     flash[:success] = 'このお仕事はお仕事リストから正常に削除されました。お疲れさまでした。'
     redirect_to tasks_url
   end
 
   private
+  
+  #共通
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
